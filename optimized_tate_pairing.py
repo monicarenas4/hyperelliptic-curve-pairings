5c5a15c51f439c9b02e2935b88ce01d0b12788c5
monicarenas4 <monicarenas4@gmail.com>
2023-11-21 12:27:54 +0100

    function

--- optimized_tate_pairing.py
+++ optimized_tate_pairing.py
@@ -5,45 +5,55 @@ from sage.rings.integer_ring import ZZ
 from sage.schemes.elliptic_curves.constructor import EllipticCurve
 from sage.rings.finite_rings.finite_field_constructor import FiniteField, GF
 
-R = QQ['x']
-(x,) = R._first_ngens(1)
 
-rx = x ** 4 - x ** 2 + 1
-tx = x + 1  # Trace polynomial
-px = (x ** 6 - 2 * x ** 5 + 2 * x ** 3 + x + 1) / 3
+def polynomials(seed_u: int):
+    """
+    seed_u: seed
+    """
 
-u = -ZZ(2 ** 63 + 2 ** 62 + 2 ** 60 + 2 ** 57 + 2 ** 48 + 2 ** 16)  # seed
+    R = QQ['x']
+    (x,) = R._first_ngens(1)
 
-r = ZZ(rx(u))
-t = ZZ(tx(u))
-p = ZZ(px(u))
-n = p - t + 1  # Order of the curve. #E = p-t+1 = hr
-h = n / r  # Co-factor
+    rx = x ** 4 - x ** 2 + 1
+    tx = x + 1  # trace polynomial
+    px = (x ** 6 - 2 * x ** 5 + 2 * x ** 3 + x + 1) / 3
 
-Fp = GF(p, proof=False)
-E = EllipticCurve([Fp(0), Fp(-3)])
-P = E.random_element()
-P = h * P
+    r = ZZ(rx(seed_u))
+    t = ZZ(tx(seed_u))
+    p = ZZ(px(seed_u))
+    n = p - t + 1  # order of the curve. #E = p-t+1 = hr
+    h = n / r  # co-factor
 
-print("Random Point:", P)
-print((P[1] ** 2 - P[0] ** 3 + 3) % p)
-print(r * P)
-# print('The order of the curve is:', n)
-# print(n%r)
-# print((p**12-1)%r)
-# print(h)
+    Fp = GF(p, proof=False)
+    E = EllipticCurve([Fp(0), Fp(-3)])
+    P = E.random_element()
+    P = h * P
 
-# print(r, is_prime(r))
-# print(t, is_prime(t))
-# print (p, is_prime(p))
+    # print("Random Point:", P)
+    # print((P[1] ** 2 - P[0] ** 3 + 3) % p)
+    # print(r * P)
+    # print('The order of the curve is:', n)
+    # print(n%r)
+    # print((p**12-1)%r)
+    # print(h)
 
-Fpw = Fp['w']
-(w,) = Fpw._first_ngens(1)
+    # print(r, is_prime(r))
+    # print(t, is_prime(t))
+    # print (p, is_prime(p))
 
-print("Fp12 = Fp[w]/(w^12 + w^6 + 2)")
-Fp12 = Fp.extension(w ** 12 + w ** 6 + 2, names=('w',))
-(w,) = Fp12._first_ngens(1)
-print(Fp12)
-E12 = EllipticCurve([Fp12(0), Fp12(-3)])
-Q = E12.random_element()
-print("The order is:", E12.order() % r)
+    Fpw = Fp['w']
+    (w,) = Fpw._first_ngens(1)
+
+    print("Fp12 = Fp[w]/(w^12 + w^6 + 2)")
+    Fp12 = Fp.extension(w ** 12 + w ** 6 + 2, names=('w',))
+    (w,) = Fp12._first_ngens(1)
+    print(Fp12)
+    E12 = EllipticCurve([Fp12(0), Fp12(-3)])
+    Q = E12.random_element()
+    print("The order is:", E12.order() % r)
+
+    return
+
+
+u = -ZZ(2 ** 63 + 2 ** 62 + 2 ** 60 + 2 ** 57 + 2 ** 48 + 2 ** 16)
+polynomials(u)
