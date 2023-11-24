6ebf2372cf59fc26cc7095dc9f482e5681bd7fe3
monicarenas4 <monicarenas4@gmail.com>
2023-11-24 09:47:24 +0100

    bilinearity check

--- optimized_tate_pairing.py
+++ optimized_tate_pairing.py
@@ -47,15 +47,15 @@ def tate_pairing(u: int, a: int = 0, b: int = -3):
     h12 = n12 / r ** 2
     Q = h12 * Q
 
-    a, b = randint(0, r - 1), randint(0, r - 1)
+    a_randint, b_randint = randint(0, r - 1), randint(0, r - 1)
 
     # pairing of P and Q
     miller_function = tate_pairing_miller_loop(P, Q, r)
     pairing_value = final_exponentiation(miller_function, p, r)
-    pairing_value = pairing_value ** (a * b)
+    pairing_value = pairing_value ** (a_randint * b_randint)
 
     # pairing of P' and Q'
-    P_prime, Q_prime = a * P, b * Q
+    P_prime, Q_prime = a_randint * P, b_randint * Q
     miller_function_prime = tate_pairing_miller_loop(P_prime, Q_prime, r)
     pairing_value_prime = final_exponentiation(miller_function_prime, p, r)
 
