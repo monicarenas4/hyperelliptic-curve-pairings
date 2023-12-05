6b6481eb65caa1b790fb8e9417ff27b778efdb1c
monicarenas4 <monicarenas4@gmail.com>
2023-12-05 14:13:29 +0100

    tate pairing

--- pairings.py
+++ pairings.py
@@ -1,37 +1,44 @@
 from sage.all_cmdline import *
-from sage.rings.integer import Integer
-from ecc_operations import ADD_step, DBL_step
+
+from generate_curves import BLS12_curve
+from miller_loop import miller_loop_tate_pairing
+from final_exponentiation import final_exponentiation_BLS12
+from correctness_tests import tate_pairing_bilinearity_check
+
+import datetime
+import time
+from os.path import exists
+from utils_ import make_folder, head_text_file
+
+TODAY = str(datetime.date.today()).replace('-', '')
+make_folder('results')
+
+txt_results = 'results' + '/' + TODAY + ' time_pairing.txt'
+head_text_file(txt_results) if not exists(txt_results) else None
 
 
-def miller_loop_tate_pairing(P, Q, r: int):
+def tate_pairing(u, check=False):
     """
-    :param P: point on the curve E(Fp)
-    :param Q: point on the extension filed E(Fp12)
-    :param r: prime number that divides the oder of the curve
-    :return: miller_function
+    :param u: curve seed
+    :return: m, pairing value
     """
-    bin_r = [int(x) for x in "{0:0b}".format(r)][1:]
-    R = P
-    miller_function = 1
+    P, Q, p, r = BLS12_curve(u)
+    t0_miller = time.time()
+    miller_function = miller_loop_tate_pairing(P, Q, r)
+    tf_miller = round(time.time() - t0_miller, 6)
 
-    for bit in bin_r:
-        R, l, v = DBL_step(Q, R)
-        miller_function = miller_function ** 2 * (l / v)
-        if bit == 1:
-            R, l, v = ADD_step(P, Q, R)
-            miller_function = miller_function * (l / v)
+    t0_pairing = time.time()
+    pairing_value = final_exponentiation_BLS12(miller_function, p, r)
+    tf_pairing = round(time.time() - t0_pairing, 6)
 
-    return miller_function
+    with open(txt_results, 'a') as file:
+        file.write(
+            f'{tf_miller}' + '\t' +
+            f'{tf_pairing}' + '\t' +
+            f'{tf_miller + tf_pairing}' +
+            '\n')
 
+    # Bilinearity check
+    tate_pairing_bilinearity_check(P, Q, pairing_value, p, r) if check == True else None
 
-def final_exponentiation(miller_fn, p, r, k: int = 12):
-    """
-    :param miller_fn: miller function
-    :param p: prime number
-    :param r: prime number
-    :param k: embedding factor
-    :return: exponentiation
-    """
-    exponentiation = miller_fn ** ((p ** k - 1) / r)
-
-    return exponentiation
+    return miller_function, pairing_value
