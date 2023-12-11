a947f80db4a7a98b3eb2fcf6d43f3e3fccf8b437
monicarenas4 <monicarenas4@gmail.com>
2023-12-11 15:48:10 +0100

    main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sage.all_cmdline import *
 from sage.rings.integer_ring import ZZ
 
-from pairings import tate_pairing, optimized_tate_pairing
+from pairings import tate_pairing, optimized_tate_pairing, optimal_ate_pairing
 import datetime
 from os.path import exists
 from utils_ import make_folder, head_text_file, write_results
@@ -11,11 +11,15 @@ make_folder('results')
 txt_results = 'results/time_pairing.txt'
 head_text_file(txt_results) if not exists(txt_results) else None
 
-# Tate Pairing
+# Tate pairing
 u = -ZZ(2 ** 63 + 2 ** 62 + 2 ** 60 + 2 ** 57 + 2 ** 48 + 2 ** 16)
 _, _, time_miller, time_pairing = tate_pairing(u, check=True)
 write_results(TODAY, txt_results, 'tate_pairing', time_miller, time_pairing)
 
-# Optimized Tate Pairing
+# Optimized tate pairing
 _, _, tf_miller, tf_total = optimized_tate_pairing(u, check=True)
 write_results(TODAY, txt_results, 'opt_tate_pairing', tf_miller, tf_total)
+
+# Optimal ate pairing
+_, _, tf_miller, tf_total = optimal_ate_pairing(u, check=True)
+write_results(TODAY, txt_results, 'opt_ate_pairing', tf_miller, tf_total)
