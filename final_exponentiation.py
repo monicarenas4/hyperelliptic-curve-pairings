19df48a94544a1b160767f7c3f9a7ea6817f2176
monicarenas4 <monicarenas4@gmail.com>
2023-12-11 15:47:35 +0100

    frobenius prop.

--- final_exponentiation.py
+++ final_exponentiation.py
@@ -20,36 +20,32 @@ def final_exp_easy_BLS12(miller_fn, p: int):
     :param p: prime number
     :return: exponentiation
     """
-    # t0_easy = time.time()
     inverse = miller_fn ** (-1)
     f = miller_fn ** (p ** 6)
     f = f * inverse
     f2 = f ** (p ** 2)
     easy_k12 = f2 * f
-    # tf_easy = round(time.time() - t0_easy, 6)
 
     return easy_k12
 
 
 def final_exp_hard_BLS12(input_fn, u, p):
-    u0 = -u
+    u0 = abs(u)
     # hard_BLS12 = miller_fn ** ((p ** 4 - p ** 2 + 1) / r)
+
     f3 = input_fn ** (u0 + 1)
     f3 = f3 ** (u0 + 1)
-    # res = f3 ** p  # Frobenius prop.
-    res = f3.frobenius(1)
+    res = f3.frobenius(1)  # f3 ** p ** 1
     f2 = f3 ** u0
-    # f2 = 1 / f2 # Frobenius prop.
-    f2 = f2.frobenius(6)
+    f2 = f2.frobenius(6)  # 1 / f2
     res = res * f2
     f1 = f2 ** u0
     f1 = f1 * f3
-    f1 = 1 / f1
+    f1 = f1.frobenius(6)  # 1 / f1
     res = res ** p
     res = res * f1
     f0 = f1 ** u0
-    # f0 = 1 / f0 # Frobenius prop.
-    f0 = f0.frobenius(6)
+    f0 = f0.frobenius(6)  # 1 / f0
     f0 = f0 * input_fn ** 2 * input_fn
     res = res ** p
     hard_BLS12 = res * f0
