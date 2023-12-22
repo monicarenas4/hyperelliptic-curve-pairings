6c3627323ce6c56deb3fcde3fcd2c52475ff1712
monicarenas4 <monicarenas4@gmail.com>
2023-12-22 14:16:00 +0100

    twisted operations

---
+++ hyperelliptic_curve/twisted_operations.py
@@ -0,0 +1,18 @@
+from operations import ADD, DBL
+from final_exponentiation import final_exponentiation
+
+
+def Twisted_Ate_k8(D, Dn, E, F, s, U, K):
+    T, fc = D, 1
+    for i in s:
+        T, lc = DBL(T, E, F)
+        fc = lc * fc ^ 2
+        if s[i] == 1:
+            T, lc = ADD(D, T, E, F)
+            fc = lc * fc
+        if s[i] == -1:
+            T, lc = ADD(Dn, T, E, F)
+            fc = lc * fc
+
+    fc = final_exponentiation(fc, U, K)
+    return fc
