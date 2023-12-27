4979ff119ba0f96eebfa03fa6a40d40ee859f703
gfotiadis <gfotiadis.crypto@gmail.com>
2023-12-27 14:11:08 +0100

    modifying algorithms

--- hyperelliptic_curve/generate_jacobian.py
+++ hyperelliptic_curve/generate_jacobian.py
@@ -1,11 +1,15 @@
-from operations import JC_random_element
+from operations import JC_random_element, HEC_random_point, ADD, DBL
 from sage.rings.finite_rings.finite_field_constructor import FiniteField, GF
 from sage.schemes.hyperelliptic_curves.constructor import HyperellipticCurve
 from sage.rings.integer_ring import ZZ
 from _utils import NAF
-
+from operations import new_coordinates
+from twisted_operations import Twisted_Ate_k8
+from random import randint
 
 def generate_jacobian(k=8, a=3):
+
+    # Hyperelliptic curve + Jacobian parameters
     p = 0x63757e4ba8c6ff6428754c24f70a9d3fe49534369f934a87b3bc1ff278656337cc69cee396a8ef98ad875836188ff0f293ae2a233bd903541cf070deadb7631ff5f27ad9
     r = 0xff0060739e18d7594a978b0ab6ae4ce3dbfd52a9d00197603fffdf0000000101
     h = 0x26cad1cf6fb1762e04b1549002acb3556aa8178f23bd901d1d01f940fb055fb7ca43e8b854a30786a394a65690a583fbb88c4c850a7fcf78daf75074603484a1c06a742ea4a9d002bf9b63808aeee5759acee12b509649987d7270d3c561273221ebfbba91d5a0c2
@@ -14,8 +18,10 @@ def generate_jacobian(k=8, a=3):
     X = 0x9f910b5ab8f0c95906b67d8867694eae3f74da06d105ba2a05e1a459b29c00a0d109
     Y = 0x13ec07890859f0d2fdd0d79e517fb6f49886c959639a1ef72cc8fd885c000012e
     T = 0xffc00020fffffffc
+    
+    l = 7457621329808235781107228406086954129386944851397679741565717026992476628562587060421593049761741274109849681179124230276920918105899662131351644732718514068050842
     NAF_T = NAF(T)
-
+    
     Zx = ZZ['t']  # Define the ring of polynomials with integer coefficients
     (t,) = Zx._first_ngens(1)
     xt = t ** 4 + 4 * Y * t ** 3 + 8 * Y ** 2 * t ** 2 + 4 * p * Y * t + p ** 2
@@ -23,6 +29,7 @@ def generate_jacobian(k=8, a=3):
 
     lx, ly = 41009, 1
     U = [T, (T // 4), lx, ly]
+    F = [0, 3, 0, 0, 0]
 
     Fp = GF(p, proof=False)  # Fix the prime field Fp
     Fpx = Fp['x']  # Fix Fpx as the ring of polynomials in x, with coefficients in Fp
@@ -35,8 +42,23 @@ def generate_jacobian(k=8, a=3):
 
     P = JC_random_element(C)
     P = h * P  # Force P to have order r
-    # Q = JC_random_element(C)
-    # h = n // r
+    
+    a = randint(0, r - 1)
+    M = a*P
+    
+    R1 = P + M
+    R2 = 2*P
+        
+    D1 = new_coordinates(P)
+    D1_ = -P
+    D1_ = new_coordinates(D1_)
+    
+    D2 = new_coordinates(M)
+    D2_ = -M
+    D2_ = new_coordinates(D2_)
+
+    Ct = HyperellipticCurve(x**5 + 3*l*x)
+    Jt = Ct.jacobian()
 
     # Constructing the curve and Jacobian over the extension field Fp**8
     # print("Fp12 = Fp[w]/(w^12 + w^6 + 2)")
@@ -44,24 +66,59 @@ def generate_jacobian(k=8, a=3):
     (w,) = Fpw._first_ngens(1)
     Fp8 = Fp.extension(w ** 8 + 3, names=('w',))  # Fp8: degree 8 extension of Fp.
     (w,) = Fp8._first_ngens(1)
-
     Fp8x = Fp8['x']
     (x,) = Fp8x._first_ngens(1)
     C8 = HyperellipticCurve(Fp8x([0, 3, 0, 0, 0, 1]))
+    
     # print(C8)
     Jac8 = C8.jacobian()
     # print(Jac8)
 
+    g = x**8 - l
+    roots = g.roots()
+    c = roots[7]
+    c = c[0]
+
+    Q = HEC_random_point(Ct)
+    xQ = Q[0]
+    yQ = Q[1]
+ 
+    Q = Ct([xQ,yQ])
+
+    xQ = xQ / (c**2)
+    yQ = yQ / (c**5)
+
+    Q = C8([xQ,yQ])
+    
+    Q = [-xQ, yQ, xQ**2, -xQ**3]
+
     Res = (t ** 8 - 1).resultant(xt)  # Compute the resultant  of the polynomials t^8 - 1 and \chi(t)
     h_ = Res // r ** 4  # Compute the cofactor of the Jacobian J8
     n_ = h_ * r  # Compute the order of the Jacobian J8
+    
+#    R, line = ADD(D1, D2, Q, F)
+#    S, line = DBL(D1, Q, F)
+#
+#    assert R1[0][0] == R[1]/R[6]
+#    assert R1[0][1] == R[0]/R[6]
+#    assert R1[1][0] == R[3]/(R[6]*R[4]*R[5])
+#    assert R1[1][1] == R[2]/(R[6]*R[4]*R[5])
+#
+#    assert R2[0][0] == S[1]/S[6]
+#    assert R2[0][1] == S[0]/S[6]
+#    assert R2[1][0] == S[3]/(S[6]*S[4]*S[5])
+#    assert R2[1][1] == S[2]/(S[6]*S[4]*S[5])
+    
+    pow = (p**k - 1)//r
+        
+    t1 = Twisted_Ate_k8(D2, D2_, Q, F, NAF_T, U, Fp, pow)
+    t2 = Twisted_Ate_k8(D1, D1_, Q, F, NAF_T, U, Fp, pow)**a
 
-    Q = JC_random_element(C8)  # Pick random element in Jacobian J8(Fp8)
-    Q = h_ * Q  # Force Q to have order r
-
-    print(P)
-    print(Q)
-
+    print(t1)
+    print(t2)
+    
+    print(t1 == t2)
+    
     return U
 
 
--- hyperelliptic_curve/operations.py
+++ hyperelliptic_curve/operations.py
@@ -1,6 +1,6 @@
 def Line(D2, C):
     x2, y2, x22, x23 = D2[0], D2[1], D2[2], D2[3]
-    l3, l2, l1, l0, l = C[0], C[1], C[2], C[3], C[5]
+    l3, l2, l1, l0, l = C[0], C[1], C[2], C[3], C[4]
     cD2 = (y2 * l + l3 * x23 - l2 * x22 + l1 * x2 - l0)
 
     return cD2
@@ -17,7 +17,7 @@ def ADD(D1, D2, E, F):
     y1, y2 = (Ut11 - U21), (U20 - Ut10)
     y3 = U11 * y1
     y4 = y2 + y3
-    r = y2 * y4 + y1 ^ 2 * U10
+    r = y2 * y4 + y1**2 * U10
     inv1, inv0 = y1, y4
     w0 = V10 * z24 - V20
     w1 = V11 * z24 - V21
@@ -26,10 +26,10 @@ def ADD(D1, D2, E, F):
     s1p = y1 * w0 + y2 * w1
     s0p = w2 - U10 * w3
     rt = r * z23
-    R = rt ^ 2
+    R = rt ** 2
     Z31 = s1p * Z21
     Z32 = rt * Z21
-    z31, z32 = (Z31 ^ 2), (Z32 ^ 2)
+    z31, z32 = (Z31 ** 2), (Z32 ** 2)
     s0tp = s0p * z21
     l2p = (s1p * U21)
     l2 = (l2p + s0tp)
@@ -40,7 +40,7 @@ def ADD(D1, D2, E, F):
     w1 = Ut11 + U21
     U31 = s1p * (2 * s0tp - s1p * y1) - z32
     l1p = l1 * s1p
-    U30 = s0tp * (s0p - 2 * s1p * U11) + s1p ^ 2 * (y3 - Ut10 - U20) + 2 * l1p + R * w1
+    U30 = s0tp * (s0p - 2 * s1p * U11) + s1p ** 2 * (y3 - Ut10 - U20) + 2 * l1p + R * w1
     w1 = l2 * s1p - U31
     V30 = U30 * w1 - z31 * l0 * s1p
     V31 = U31 * w1 + z31 * (U30 - l1p)
@@ -59,14 +59,14 @@ def DBL(D1, E, F):
     Vt11 = 2 * V11
     Vt10 = 2 * V10
     zt11 = Vt10 * z11
-    w0 = V11 ^ 2
-    w1 = U11 ^ 2
+    w0 = V11 ** 2
+    w1 = U11 ** 2
     w2 = 4 * w0
     w3 = zt11 - U11 * Vt11
     r = Ut10 * w2 + zt11 * w3
     inv1p = -Vt11
     inv0p = w3
-    z11p = z11 ^ 2
+    z11p = z11 ** 2
     z11pp = z11 * z11p
     w3 = f3 * z11p + w1
     w4 = 2 * Ut10
@@ -78,11 +78,11 @@ def DBL(D1, E, F):
     s0p = w0 - Ut10 * w1
     z13 = Z11 * Z12
     rt = r * z13
-    R = rt ^ 2
+    R = rt ** 2
     Z31 = s1p * Z11
     Z32 = rt * Z11
-    z31 = Z31 ^ 2
-    z32 = Z32 ^ 2
+    z31 = Z31 ** 2
+    z32 = Z32 ** 2
     st0p = s0p * z11
     l2p = s1p * U11
     l2 = l2p + st0p
@@ -98,6 +98,7 @@ def DBL(D1, E, F):
     V30 = U30 * w1 - z31 * l0 * s1p
     V31 = U31 * w1 + z31 * (U30 - l1 * s1p)
     line = [l3, l2, l1, l0, l]
+    
     D3 = [U31, U30, V31, V30, Z31, Z32, z31, z32]
     lcE = Line(E, line)
 
@@ -109,16 +110,13 @@ def new_coordinates(D):
     :param D:
     :return:
     """
-    u, v = D[0], D[1]
-    # how to extract the coefficients in Sage?
-    Cu, Cv = Coefficients(u), Coefficients(v)
-    U1, U0 = Cu[1], Cu[0]
-    V1, V0 = Cv[1], Cv[0]
+    U1, U0 = D[0][1], D[0][0]
+    V1, V0 = D[1][1], D[1][0]
     Z1, Z2 = (1), (1)
-    z1, z2 = (Z1 ^ 2), (Z2 ^ 2)
-    Dnew = [U1, U0, V1, V0, Z1, Z2, z1, z2]
+    z1, z2 = (Z1**2), (Z2**2)
+    D_ = [U1, U0, V1, V0, Z1, Z2, z1, z2]
 
-    return Dnew
+    return D_
 
 
 def HEC_random_point(C):
@@ -158,4 +156,4 @@ def JC_random_element(C):
     v = f.parent().lagrange_polynomial(points)
 
     return J([u, v])
-    # return 1
+
