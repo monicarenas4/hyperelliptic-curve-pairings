9314f668d13adbdbaee37a1e623ada42be09a763
monicarenas4 <monicarenas4@gmail.com>
2024-01-05 16:49:38 +0100

    update miller function inputs

--- hyperelliptic_curve/pairing_types.py
+++ hyperelliptic_curve/pairing_types.py
@@ -16,11 +16,10 @@ def twisted_ate_cp8(P, Q, Q_prec, c_vec, F, length_miller, U, K, case: str = 'ca
     :param NAF_rep: true or false
     :return:
     """
-    if NAF_rep == False:
+    if not NAF_rep:
         miller_fun = miller_function(P, Q, Q_prec, c_vec, F, length_miller, case)
     else:
-        P_neg = [P[0], P[1], -P[2], -P[3], P[4], P[5], P[6], P[7]]
-        miller_fun = miller_function(P, Q, Q_prec, c_vec, F, length_miller, case, P_neg=P_neg)
+        miller_fun = miller_function(P, Q, Q_prec, c_vec, F, length_miller, case, NAF_rep=NAF_rep)
 
     pairing_value = final_exponentiation_cp8(miller_fun, U, K)
 
@@ -44,8 +43,7 @@ def ate_i(Q, P, P_prec, c_vec, F, length_miller, U, W, case: str = 'case1', NAF_
     if NAF_rep == False:
         miller_fun = miller_function(Q, P, P_prec, c_vec, F, length_miller, case, twist='k16')
     else:
-        Q_neg = [Q[0], Q[1], -Q[2], -Q[3], Q[4], Q[5], Q[6], Q[7]]
-        miller_fun = miller_function(Q, P, P_prec, c_vec, F, length_miller, case, twist='k16', P_neg=Q_neg)
+        miller_fun = miller_function(Q, P, P_prec, c_vec, F, length_miller, case, twist='k16', NAF_rep=NAF_rep)
 
     pairing_value = final_exponentiation_k16(miller_fun, U, W)
 
