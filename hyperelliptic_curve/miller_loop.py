cfebaac065a8ed6b63230ea2d322007793e2a07b
monicarenas4 <monicarenas4@gmail.com>
2024-01-05 16:47:38 +0100

    NEG value

--- hyperelliptic_curve/miller_loop.py
+++ hyperelliptic_curve/miller_loop.py
@@ -3,7 +3,7 @@ from jacobian_operations import ADD, DBL
 from _utils import NAF
 
 
-def miller_function(P, Q, Q_prec, c_vec, F, length_miller, case: str, twist: str = None, P_neg=None):
+def miller_function(P, Q, Q_prec, c_vec, F, length_miller, case: str, twist: str = None, NAF_rep=False):
     """
     :param P:
     :param Q:
@@ -13,15 +13,16 @@ def miller_function(P, Q, Q_prec, c_vec, F, length_miller, case: str, twist: str
     :param length_miller:
     :param case: case1 => degenerate divisor or case2 => general divisor
     :param twist:
-    :param P_neg:
+    :param NAF_rep:
     :return:
     """
-    if P_neg == None:
+    if not NAF_rep:
         length_miller = Integer(length_miller).digits(2)
     else:
         length_miller = NAF(length_miller)
 
     T, fc = P, 1
+    P_neg = [P[0], P[1], -P[2], -P[3], P[4], P[5], P[6], P[7]]
 
     for i in range(len(length_miller) - 2, -1, -1):
         T, lc = DBL(T, Q_prec, Q, F, c_vec, case=case, twist=twist)
