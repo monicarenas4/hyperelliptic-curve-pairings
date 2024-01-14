e6b5783e34b558d802aab1acf5d3288145e6e90d
Monica Arenas <monicarenas4@gmail.com>
2024-01-14 10:09:43 +0100

    unify examples for k = 16

--- hyperelliptic_curve/main.py
+++ hyperelliptic_curve/main.py
@@ -1,9 +1,8 @@
 from sage.rings.integer_ring import ZZ
+import datetime
 
 from generate_jacobian_cp8 import generate_jacobian
 from generate_jacobian_k16 import generate_jacobian_k16
-from generate_jacobian_new_k16 import generate_jacobian_new_k16
-import datetime
 from _utils import make_folder
 from write_number_operations import operations_main, write_number_operations_head
 
@@ -22,6 +21,6 @@ u = ZZ(0x100004003)
 generate_jacobian_k16(u, family='k16')
 
 print('+++++++++++++++++++++\nExample Jacobian from new family for k = 16\n+++++++++++++++++++++')
-# operations_main(file_name, 'Jacobian new family k16')
+operations_main(file_name, 'Jacobian new family k16')
 u = ZZ(0xeffeffff7fff)
 generate_jacobian_k16(u, family='new_k16')
