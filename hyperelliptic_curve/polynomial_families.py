8e391a9b2dd69ee1055c98b11a742e1de9fa1d44
monicarenas4 <monicarenas4@gmail.com>
2024-02-16 09:03:01 +0100

    family k24

--- hyperelliptic_curve/polynomial_families.py
+++ hyperelliptic_curve/polynomial_families.py
@@ -42,3 +42,26 @@ def polynomial_family_new_k16(u):
     Y = ZZ(Yx(u))
 
     return r, p, X, Y
+
+
+def polynomial_family_k24(u):
+    """
+    :param u: seed
+    :return: r, p, X, Y
+    """
+
+    R = QQ['x']
+    (x,) = R._first_ngens(1)
+    rx = x ** 8 - x ** 4 + 1
+    px = (2 * x ** 12 + 4 * x ** 11 + 3 * x ** 10 - 2 * x ** 9 - x ** 8 + 4 * x ** 7 - 3 * x ** 6 + 2 * x ** 5 +
+          x ** 4 - 4 * x ** 3 + 3 * x ** 2 - 2 * x + 1) / 8
+    Xx = -(x ** 6 + x ** 5) / 2
+    Yx = (x ** 5 - x ** 4 - x ** 3 + x ** 2 - x + 1) / 4
+    p2x = Xx ** 2 + 2 * Yx ** 2
+
+    r = ZZ(rx(u))
+    p = ZZ(px(u))
+    X = ZZ(Xx(u))
+    Y = ZZ(Yx(u))
+
+    return r, p, X, Y
