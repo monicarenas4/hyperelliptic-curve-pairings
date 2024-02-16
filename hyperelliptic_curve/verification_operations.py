db842b3003fb46b349fa6ce80c119180c7480879
monicarenas4 <monicarenas4@gmail.com>
2024-02-16 09:03:56 +0100

    updates

--- hyperelliptic_curve/verification_operations.py
+++ hyperelliptic_curve/verification_operations.py
@@ -5,7 +5,7 @@ from pairing_types import twisted_ate_cp8, ate_i
 from random import randint
 
 
-def test_bilinearity_Twisted_Ate(curves, jacobians, fields, c_vec, F, U, p, r, h, h_, length_miller):
+def test_bilinearity_Twisted_Ate(curves, jacobians, fields, c_vec, F, U, p, r, h, h_, length_miller, k: int):
     """
     :param curves:
     :param jacobians:
@@ -18,12 +18,12 @@ def test_bilinearity_Twisted_Ate(curves, jacobians, fields, c_vec, F, U, p, r, h
     :param h:
     :param h_:
     :param length_miller:
+    :param k: embeding degree
     :return:
     """
     C, Ct, C8 = curves[0], curves[1], curves[2]
     J, Jt, J8 = jacobians[0], jacobians[1], jacobians[2]
     Fp, Fp8 = fields[0], fields[1]
-    c, c2, c3, c4, c5, c6, c7 = c_vec[0], c_vec[1], c_vec[2], c_vec[3], c_vec[4], c_vec[5], c_vec[6]
 
     P2 = JC_random_element(C)
     P2 = h * P2  # Force P to have order r
@@ -54,20 +54,16 @@ def test_bilinearity_Twisted_Ate(curves, jacobians, fields, c_vec, F, U, p, r, h
             Q1_prec, _, _ = precomputation_general_div(Q1)
 
         print('----------------\nCASE: %s\n----------------' % case)
-        pairing_value1 = twisted_ate_cp8(P1, Q1, Q1_prec, c_vec, F, length_miller, U, Fp, k=8, case=case)
-        pairing_value2 = twisted_ate_cp8(P2, Q2, Q2_prec, c_vec, F, length_miller, U, Fp, k=8,
+        pairing_value1 = twisted_ate_cp8(P1, Q1, Q1_prec, c_vec, F, length_miller, U, Fp, k=k, case=case)
+        pairing_value2 = twisted_ate_cp8(P2, Q2, Q2_prec, c_vec, F, length_miller, U, Fp, k=k,
                                          case=case) ** (randint_P * randint_Q)
-        #        print('pairing value 1 = ', pairing_value1)
-        #        print('pairing value 2 = ', pairing_value2)
         print('bilinearity test:',
               pairing_value1 == pairing_value2) if (pairing_value1 and pairing_value2) != 1 else print('review code')
 
-        pairing_value_naf1 = twisted_ate_cp8(P1, Q1, Q1_prec, c_vec, F, length_miller, U, Fp, k=8,
+        pairing_value_naf1 = twisted_ate_cp8(P1, Q1, Q1_prec, c_vec, F, length_miller, U, Fp, k=k,
                                              case=case, NAF_rep=True)
-        pairing_value_naf2 = twisted_ate_cp8(P2, Q2, Q2_prec, c_vec, F, length_miller, U, Fp, k=8,
+        pairing_value_naf2 = twisted_ate_cp8(P2, Q2, Q2_prec, c_vec, F, length_miller, U, Fp, k=k,
                                              case=case, NAF_rep=True) ** (randint_P * randint_Q)
-        # print('pairing value NAF 1 = ', pairing_value_naf1)
-        # print('pairing value NAF 2 = ', pairing_value_naf2)
         print('bilinearity NAF test:',
               pairing_value_naf1 == pairing_value_naf2) if (pairing_value_naf1 and pairing_value_naf2) != 1 \
             else print('review code')
@@ -75,7 +71,7 @@ def test_bilinearity_Twisted_Ate(curves, jacobians, fields, c_vec, F, U, p, r, h
     return None
 
 
-def test_bilinearity_Ate_i(curves, jacobians, fields, c_vec, F, U, W, p, r, h, h_, length_miller, family='k16'):
+def test_bilinearity_Ate_i(curves, jacobians, fields, c_vec, F, U, W, p, r, h, h_, length_miller, k: int, family='k16'):
     """
     :param curves:
     :param jacobians:
@@ -89,14 +85,13 @@ def test_bilinearity_Ate_i(curves, jacobians, fields, c_vec, F, U, W, p, r, h, h
     :param h:
     :param h_:
     :param length_miller:
+    :param k: embeding degree
+    :param family:
     :return:
     """
     C, Ct, C16 = curves[0], curves[1], curves[2]
     J, Jt, J16 = jacobians[0], jacobians[1], jacobians[2]
     Fp, Fp2, Fq8 = fields[0], fields[1], fields[2]
-    c, c2, c3, c4, c5 = c_vec[0], c_vec[1], c_vec[2], c_vec[3], c_vec[4]
-    c6, c7, c8, c9, c10 = c_vec[5], c_vec[6], c_vec[7], c_vec[8], c_vec[9]
-    c11, c12, c13, c14, c15 = c_vec[10], c_vec[11], c_vec[12], c_vec[13], c_vec[14]
 
     Q2 = JC_random_element(Ct)
     Q2 = h_ * Q2  # Force P to have order r
@@ -110,11 +105,9 @@ def test_bilinearity_Ate_i(curves, jacobians, fields, c_vec, F, U, W, p, r, h, h
 
     for case in cases:
         if case == 'case1':
-            # case 1 => Degenerate Divisor
             P = HEC_random_point(C)
             xP, yP = P[0], P[1]
             P = C([xP, yP])
-
             P1 = P
             P1_prec, _, _ = precomputation_degenerate_div(P1)
             P2 = P1
@@ -124,27 +117,22 @@ def test_bilinearity_Ate_i(curves, jacobians, fields, c_vec, F, U, W, p, r, h, h
             P = JC_random_element(C)
             P2 = h * P
             P2_prec, _, _ = precomputation_general_div(P2)
-
             randint_P = randint(0, r - 1)
             P1 = randint_P * P2
             P1_prec, _, _ = precomputation_general_div(P1)
 
         print('----------------\nCASE: %s\n----------------' % case)
 
-        pairing_value1 = ate_i(Q1, P1, P1_prec, c_vec, F, length_miller, U, W, k=16, case=case, family=family)
-        pairing_value2 = ate_i(Q2, P2, P2_prec, c_vec, F, length_miller, U, W, k=16, case=case,
+        pairing_value1 = ate_i(Q1, P1, P1_prec, c_vec, F, length_miller, U, W, k=k, case=case, family=family)
+        pairing_value2 = ate_i(Q2, P2, P2_prec, c_vec, F, length_miller, U, W, k=k, case=case,
                                family=family) ** (randint_Q * randint_P)
-        # print('pairing value 1 = ', pairing_value1)
-        # print('pairing value 2 = ', pairing_value2)
         print('bilinearity test:',
               pairing_value1 == pairing_value2) if (pairing_value1 and pairing_value2) != 1 else print('review code')
 
-        pairing_value_naf1 = ate_i(Q1, P1, P1_prec, c_vec, F, length_miller, U, W, k=16, case=case, NAF_rep=True,
+        pairing_value_naf1 = ate_i(Q1, P1, P1_prec, c_vec, F, length_miller, U, W, k=k, case=case, NAF_rep=True,
                                    family=family)
-        pairing_value_naf2 = ate_i(Q2, P2, P2_prec, c_vec, F, length_miller, U, W, k=16, case=case, NAF_rep=True,
+        pairing_value_naf2 = ate_i(Q2, P2, P2_prec, c_vec, F, length_miller, U, W, k=k, case=case, NAF_rep=True,
                                    family=family) ** (randint_Q * randint_P)
-        # print('pairing value NAF 1 = ', pairing_value_naf1)
-        # print('pairing value NAF 2 = ', pairing_value_naf2)
         print('bilinearity NAF test:',
               pairing_value_naf1 == pairing_value_naf2) if pairing_value_naf1 != 1 and pairing_value_naf2 != 1 \
             else print('review code')
